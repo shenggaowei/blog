fd570d2b1a8c5925f07061f9a36c97b86346162e
weishenggao <weishenggao@innobuddy.com>
2021-02-04 17:35:37 +0800

    fix: 添加canvas

--- docs/.vuepress/catalog/fronted/rn.js
+++ docs/.vuepress/catalog/fronted/rn.js
@@ -2,7 +2,8 @@ const { parsedArticleName } = require('../lib');
 
 const prefix = '/fronted/react_native/';
 const data = [
-  'animated'
+  'animated',
+  '问题记录'
 ]
 
 module.exports = parsedArticleName(prefix, data);
