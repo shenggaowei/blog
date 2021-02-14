0ba9f6b3a3fba313f81c444c3c3d54385718aa84
weishenggao <>
2021-02-14 22:10:55 +0800

    添加《兄弟》读后感

--- docs/.vuepress/catalog/read_travel/note.js
+++ docs/.vuepress/catalog/read_travel/note.js
@@ -2,7 +2,8 @@ const { parsedArticleName } = require('../lib');
 
 const prefix = '/note/';
 const data = [
-    '文化苦旅'
+    '文化苦旅',
+    '兄弟'
 ]
 
 module.exports = parsedArticleName(prefix, data);
