08eb3956ee0f42bb5b01d920e5610af624f6b9ba
weishenggao <>
2021-02-15 20:50:27 +0800

    在细雨中呐喊

--- docs/.vuepress/catalog/read_travel/note.js
+++ docs/.vuepress/catalog/read_travel/note.js
@@ -3,7 +3,8 @@ const { parsedArticleName } = require('../lib');
 const prefix = '/note/';
 const data = [
     '文化苦旅',
-    '兄弟'
+    '兄弟',
+    '在细雨中呐喊'
 ]
 
 module.exports = parsedArticleName(prefix, data);
