0a48034acc2c365b255b7e95646e5ec80e6a58fc
weishenggao <>
2021-04-01 00:18:33 +0800

    feat: 引入 gittalk

---
+++ docs/.vuepress/components/comment.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="gitalk-container">
+    <div id="gitalk-container"></div>
+  </div>
+</template>
+
+<script>
+import "gitalk/dist/gitalk.css";
+import Gitalk from "gitalk";
+
+export default {
+  name: "comment",
+  mounted() {
+    const commentConfig = {
+      clientID: "1b4d138c1587ac0bc41d",
+      clientSecret: "4fd806e27db852913534fe03b83cd58644ecd609",
+      repo: "blog",
+      owner: "shenggaogao",
+      admin: ["shenggaogao"],
+      id: window.location.pathname,
+      title: location.pathname,
+    };
+    const gitalk = new Gitalk(commentConfig);
+    gitalk.render("gitalk-container");
+  },
+};
+</script>
--- docs/.vuepress/utils/plugins.js
+++ docs/.vuepress/utils/plugins.js
@@ -1,5 +1,6 @@
 module.exports = [
   '@vuepress/back-to-top',
   '@vuepress/medium-zoom',
+  '@vuepress/nprogress',
   'reading-progress'
 ];
