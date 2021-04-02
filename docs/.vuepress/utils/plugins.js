152d5fab596617980fdf47bc0926c3124cf2a37e
weishenggao <weishenggao@innobuddy.com>
2021-04-02 16:59:12 +0800

    add vssue

--- docs/.vuepress/utils/plugins.js
+++ docs/.vuepress/utils/plugins.js
@@ -7,12 +7,17 @@ module.exports = [
     '@vuepress/blog',
     {
       comment: {
+        platform: 'github',
         service: 'vssue',
         owner: 'shenggaogao',
         repo: 'blog',
         clientId: '1b4d138c1587ac0bc41d',
         clientSecret: '4fd806e27db852913534fe03b83cd58644ecd609',
       },
+      sitemap: {
+        hostname: 'https://shenggaogao.github.io/blog/'
+      },
     },
+    
   ],
 ];
