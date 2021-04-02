e436a62542b0dd60a5ab2cf1d6eb45376a85f76a
weishenggao <weishenggao@innobuddy.com>
2021-04-02 16:21:52 +0800

    gittalk

--- docs/.vuepress/theme/components/comment.vue
+++ docs/.vuepress/theme/components/comment.vue
@@ -18,7 +18,6 @@ export default {
   },
   methods: {
     initGitTalk() {
-      const { title = "" } = this.$page;
       const commentConfig = {
         clientID: "1b4d138c1587ac0bc41d",
         clientSecret: "4fd806e27db852913534fe03b83cd58644ecd609",
@@ -28,6 +27,7 @@ export default {
         labels: ["gittalk"],
         id: title.replace(/\s+/g, ""),
         name: "大芒果的博客",
+        createIssueManually: true,
       };
       const gitalk = new Gitalk(commentConfig);
       gitalk.render("gitalk-container");
