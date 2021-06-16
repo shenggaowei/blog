f745ea50bea06826201ef945133ec476dcea0b96
gaogao <weishenggao@innobuddy.com>
2021-06-16 15:06:55 +0800

    音调调研结果

--- docs/.vuepress/catalog/fronted/rn.js
+++ docs/.vuepress/catalog/fronted/rn.js
@@ -4,7 +4,8 @@ const prefix = '/fronted/react_native/';
 const data = [
   'animated',
   '问题记录',
-  'video_coding'
+  'video_coding',
+  'audio'
 ]
 
 module.exports = parsedArticleName(prefix, data);
