c8a7560bb1226ed39a52503ec09a773a27b7ea37
weishenggao <weishenggao@innobuddy.com>
2021-07-17 19:14:52 +0800

    删除 upload.js

--- upload.js
+++
@@ -1,6 +0,0 @@
-const PicGo = require('picgo')
-const picGo = new PicGo() // 将使用默认的配置文件：~/.picgo/config.json
-
-// 上传具体路径下的图片
-const images = process.argv.filter((val, index) => index > 1)
-picGo.upload(images)
