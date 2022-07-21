14c58ab7fa06b52ccf1dabcd70d4f5c42e5a1ca1
weishenggao <527518190@qq.com>
2022-07-21 19:12:41 +0800

    update tdk

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,11 +118,8 @@ module.exports = {
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#ffffff`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/head.jpeg`, 
       },
     },
     `gatsby-plugin-react-helmet`,
